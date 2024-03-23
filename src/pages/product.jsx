cd1c7caa72e7361eaac3d593b8af4999962da596
cndhyaa <sndhyaandhya043@gmail.com>
2024-03-23 23:26:05 +0545

    productupdated

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -24,29 +24,29 @@ export default function product() {
         {!foods.length && "no data"}
         {foods.length && foods.map((food, index) => {
           return <div className="col-md" key={index}>
-            <img src={'http://localhost:8000/static/' + food.image} alt="" width="900" height="700" />
+            <img src={'http://localhost:8000/static/' + food.image} alt="" width="600" height="350" />
             <br />
 
             <h2>{food.name}</h2>
             <h5>Price Rs.{food.price}</h5>
 
-                  <div class="row">
-                    <div class="col">
-                      <button type="button" className="btn btn-success rounded-pill"><ion-icon name="cart-outline"></ion-icon>Add to cart</button>
-                      {/* <input type="text" class="form-control" placeholder="First name" aria-label="First name"> */}
-                    </div>
-                    <div class="col">
-                    <button type="button" className="btn btn-danger rounded-pill">Buy now</button>
-                      <br /><br />
-                      {/* <input type="text" class="form-control" placeholder="Last name" aria-label="Last name"> */}
-                    </div>
-                  </div>
-              <h3>Description:</h3>
-              <p>{food.description} </p>
+            <div class="row">
+              <div class="col">
+                <button type="button" className="btn btn-success rounded-pill"><ion-icon name="cart-outline"></ion-icon>Add to cart</button>
+                {/* <input type="text" class="form-control" placeholder="First name" aria-label="First name"> */}
+              </div>
+              <div class="col">
+                {/* <button type="button" className="btn btn-danger rounded-pill">Buy now</button> */}
+                <br /><br />
+                {/* <input type="text" class="form-control" placeholder="Last name" aria-label="Last name"> */}
+              </div>
+            </div>
+            <h3>Description:</h3>
+            <p>{food.description} </p>
 
-              <h3>Ingredients:</h3>
-              <p>{food.ingredients}</p>
-          
+            <h3>Ingredients:</h3>
+            <p>{food.ingredients}</p>
+
             <br />
           </div>
         })}
