dfe1f9c3db38527a8a358437583836ded1647aa3
cndhyaa <sndhyaandhya043@gmail.com>
2024-04-08 19:08:23 +0545

    cart updated

--- src/utilities/cart.js
+++ src/utilities/cart.js
@@ -1,23 +1,62 @@
 const addToCart = (food) => {
   //getting data from localStorage
-  const oldData = JSON.parse(localStorage.getItem("food"));
-  const data = [{
-    id: food._id,
-    name: food.name,
+  const oldData = JSON.parse(localStorage.getItem("food")); /// 123
+  const newData = [{
+    id: food._id, /// newData[0].id -> 123
+    name: food.name, /// chicken
     price: food.price,
     quantity: 1,
     image: food.image
   }]
 
-  if (oldData === null) {
-
-    localStorage.setItem("food", JSON.stringify(data));
+  if (!oldData) {
+    localStorage.setItem("food", JSON.stringify(newData));
   } else {
-    const newData = [ ...data, ...oldData ]
-    localStorage.setItem("food", JSON.stringify(newData))
-    console.log('hami ya xam')
-    console.log('newData', newData)
+    const latestData = [];
+    /**
+     *  old ['🍎', '🍌']
+     * 
+     * case 1: new ['🍎']
+     * output: ['🍎🍎', '🍌']
+     * 
+     * case 2: new ['🍉']
+     * output: ['🍎', '🍌']
+     */
+
+    // !exits -> latest = [..old, ...new]
+    // exists -> 
+    // temp data = [()]
+
+    // oldData.map((datum) => {
+    //   if(datum.id!==newData[0].id){
+    //     latestData.push(datum);
+    //   } else {
+    //     let oldQuantity = datum.quantity
+    //     let increaseQuantity = oldQuantity+1
+    //     latestData.push({
+    //       id:newData[0].id,
+    //       name:newData[0].name, 
+    //       price:newData[0].price,
+    //       image:newData[0].image,
+    //       quantity: increaseQuantity
+
+    //     })
+    //   }
+    // })
+
+    newData.forEach((newItem) => {
+      // Check if newItem exists in oldData
+      const existingItem = oldData.find(item => item.id === newItem.id);
+      if (existingItem) {
+        // If item exists, increment quantity
+        existingItem.quantity++;
+      } else {
+        // If item does not exist, add it to oldData
+        oldData.push(newItem);
+      }
+    });
+    localStorage.setItem("food", JSON.stringify(oldData))
   }
 }
 
-export {addToCart}
+export { addToCart }
